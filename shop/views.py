98748b1a8ec1f0cb8d2dec0956c727327200bbb6
wanjirumw-ui <wanjirumw123@gmail.com>
2026-06-04 15:04:26 +0300

    django project

---
+++ shop/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def shop(request):
+  template = loader.get_template('shop.html')
+  return HttpResponse(template.render())
+
+
+
+  
